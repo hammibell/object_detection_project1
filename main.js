5515fbe8f02757b4a9acaa0eb5e9a608da99e365
hammibell <94499945+hammibell@users.noreply.github.com>
2023-03-11 15:38:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function bedroom(){
+window.Location("bedroom.html")
+}
+function tv(){
+window.Location("tv.html");
+}
+function desk(){
+window.Location("desk.html")
+}
+function bottles(){
+window.Location("bottles.html")
+}
